25ecba413091a2b9ed565e4b6c8926c236a1e0d6
drew <buzzinbubba@gmail.com>
2021-08-29 20:09:55 -0600

    Fixed bug 1

---
+++ cmake-build-debug/CMakeFiles/project0.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/Book.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/Book.cpp.o"
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/BookCreator.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/BookCreator.cpp.o"
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/BookListCreator.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/BookListCreator.cpp.o"
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/Info.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/Info.cpp.o"
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/Library.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/Library.cpp.o"
+  "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/main.cpp" "/Users/drew/Documents/Soph_Year/Fall/CS_236/project0/cmake-build-debug/CMakeFiles/project0.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
